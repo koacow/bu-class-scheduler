86796e193cf70a690abf5427f06e243efd40aeb4
koacow <khoa.cnd@gmail.com>
2024-09-17 15:02:45 -0400

    added basic outline to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import { RouterProvider, Route, createRoutesFromElements, createBrowserRouter, Router } from 'react-router-dom'
+import { RouterProvider, Route, createRoutesFromElements, createBrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider, CssBaseline, responsiveFontSizes } from '@mui/material';
+import MainTemplate from './pages/MainTemplate';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 import './App.css'
 
 function App() {
+  const theme = responsiveFontSizes(createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#ff5722',
+      },
+      secondary: {
+        main: '#ff5722',
+      },
+    },
+  }))
+
   const router = createBrowserRouter(createRoutesFromElements([
-    <Route path='/' element={} />,
-    <Route path='/about' element={} />,
-    <Route path='*' element={} />,
+    <Route path='/' element={<MainTemplate />}>
+      <Route index element={<Home />} />
+    </Route>,
+    <Route path='/about' element={<About />} />,
+    <Route path='*' element={<NotFound />} />,
   ]))
   return (
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
 
